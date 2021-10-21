69be621bf266ffd9262d8e681ceb6ee26a33f0dc
X.F.Zhou <mrchip.zxf@outlook.com>
2021-10-21 17:35:09 +0800

    FEAT: initialize the project

---
+++ Simple.Format.Bmp/Test.AppendContent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Test.AppendContent
+{
+    class Program
+    {
+        static readonly string assetsName = "sample_256_126.bmp";
+        static readonly string copyName = "sample_256_126_append.bmp";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Append Hello World to bmp data!");
+            try
+            {
+                if (File.Exists(copyName))
+                {
+                    File.Delete(copyName);
+                }
+                File.Copy(assetsName, copyName);
+                StreamWriter streamWriter = File.AppendText(copyName);
+                for (int i = 0; i < 1000; ++i)
+                    streamWriter.WriteLine("Hello World!");
+                streamWriter.Close();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Simple.Format.Bmp/Test.ChangeExtention/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Test.ChangeExtention
+{
+    class Program
+    {
+        static readonly string assetsName = "sample_256_126.bmp";
+        static readonly string copyName = "sample_256_126.jpg";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Change the extension from bmp to jpeg!");
+            if (File.Exists(copyName))
+            {
+                File.Delete(copyName);
+            }
+
+            File.Copy(assetsName, copyName);
+        }
+    }
+}
